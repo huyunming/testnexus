0202c13b9a1a53671e0970f93c20b5cf2c5fb940
Administrator <abc@163.com>
2019-03-28 18:19:35 +0800

    nexus测试

---
+++ src/main/java/com/bjsxt/App.java
@@ -0,0 +1,14 @@
+package com.bjsxt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Nexus!" );
+        System.out.println( "Hello World!" );
+    }
+}
